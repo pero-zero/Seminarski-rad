0251cfecb2ef05c65f4af4f73107a5b09c4fa3d9
Nikola Horvat <nihorvat@vsite.hr>
2018-11-17 12:32:53 +0100

    Add project files.

---
+++ udaljenost između dviju točaka/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace udaljenost_između_dviju_točaka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Koordinatni_sustav = new System.Windows.Forms.Panel();
+            this.X1_vrijednost = new System.Windows.Forms.Label();
+            this.X1_koordianta = new System.Windows.Forms.TextBox();
+            this.Y1_vrijednost = new System.Windows.Forms.Label();
+            this.Y1_koordinata = new System.Windows.Forms.TextBox();
+            this.X2_vrijednost = new System.Windows.Forms.Label();
+            this.X2_koordinata = new System.Windows.Forms.TextBox();
+            this.Y2_vrijednost = new System.Windows.Forms.Label();
+            this.Y2_koordinata = new System.Windows.Forms.TextBox();
+            this.Udaljenost_vrijednost = new System.Windows.Forms.Label();
+            this.Udaljenost_koordinata = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Koordinatni_sustav
+            // 
+            this.Koordinatni_sustav.AutoSize = true;
+            this.Koordinatni_sustav.BackColor = System.Drawing.Color.White;
+            this.Koordinatni_sustav.Location = new System.Drawing.Point(12, 12);
+            this.Koordinatni_sustav.Name = "Koordinatni_sustav";
+            this.Koordinatni_sustav.Size = new System.Drawing.Size(700, 310);
+            this.Koordinatni_sustav.TabIndex = 0;
+            this.Koordinatni_sustav.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Koordinatni_sustav_MouseDown);
+            this.Koordinatni_sustav.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Koordinatni_sustav_MouseUp);
+            // 
+            // X1_vrijednost
+            // 
+            this.X1_vrijednost.AutoSize = true;
+            this.X1_vrijednost.Location = new System.Drawing.Point(13, 332);
+            this.X1_vrijednost.Name = "X1_vrijednost";
+            this.X1_vrijednost.Size = new System.Drawing.Size(23, 13);
+            this.X1_vrijednost.TabIndex = 1;
+            this.X1_vrijednost.Text = "X1:";
+            // 
+            // X1_koordianta
+            // 
+            this.X1_koordianta.Location = new System.Drawing.Point(42, 329);
+            this.X1_koordianta.Name = "X1_koordianta";
+            this.X1_koordianta.ReadOnly = true;
+            this.X1_koordianta.Size = new System.Drawing.Size(44, 20);
+            this.X1_koordianta.TabIndex = 2;
+            // 
+            // Y1_vrijednost
+            // 
+            this.Y1_vrijednost.AutoSize = true;
+            this.Y1_vrijednost.Location = new System.Drawing.Point(92, 332);
+            this.Y1_vrijednost.Name = "Y1_vrijednost";
+            this.Y1_vrijednost.Size = new System.Drawing.Size(23, 13);
+            this.Y1_vrijednost.TabIndex = 3;
+            this.Y1_vrijednost.Text = "Y1:";
+            // 
+            // Y1_koordinata
+            // 
+            this.Y1_koordinata.Location = new System.Drawing.Point(121, 329);
+            this.Y1_koordinata.Name = "Y1_koordinata";
+            this.Y1_koordinata.ReadOnly = true;
+            this.Y1_koordinata.Size = new System.Drawing.Size(44, 20);
+            this.Y1_koordinata.TabIndex = 4;
+            // 
+            // X2_vrijednost
+            // 
+            this.X2_vrijednost.AutoSize = true;
+            this.X2_vrijednost.Location = new System.Drawing.Point(172, 332);
+            this.X2_vrijednost.Name = "X2_vrijednost";
+            this.X2_vrijednost.Size = new System.Drawing.Size(23, 13);
+            this.X2_vrijednost.TabIndex = 5;
+            this.X2_vrijednost.Text = "X2:";
+            // 
+            // X2_koordinata
+            // 
+            this.X2_koordinata.Location = new System.Drawing.Point(201, 329);
+            this.X2_koordinata.Name = "X2_koordinata";
+            this.X2_koordinata.ReadOnly = true;
+            this.X2_koordinata.Size = new System.Drawing.Size(44, 20);
+            this.X2_koordinata.TabIndex = 6;
+            // 
+            // Y2_vrijednost
+            // 
+            this.Y2_vrijednost.AutoSize = true;
+            this.Y2_vrijednost.Location = new System.Drawing.Point(251, 332);
+            this.Y2_vrijednost.Name = "Y2_vrijednost";
+            this.Y2_vrijednost.Size = new System.Drawing.Size(23, 13);
+            this.Y2_vrijednost.TabIndex = 7;
+            this.Y2_vrijednost.Text = "Y2:";
+            // 
+            // Y2_koordinata
+            // 
+            this.Y2_koordinata.Location = new System.Drawing.Point(280, 329);
+            this.Y2_koordinata.Name = "Y2_koordinata";
+            this.Y2_koordinata.ReadOnly = true;
+            this.Y2_koordinata.Size = new System.Drawing.Size(44, 20);
+            this.Y2_koordinata.TabIndex = 8;
+            // 
+            // Udaljenost_vrijednost
+            // 
+            this.Udaljenost_vrijednost.AutoSize = true;
+            this.Udaljenost_vrijednost.Location = new System.Drawing.Point(388, 332);
+            this.Udaljenost_vrijednost.Name = "Udaljenost_vrijednost";
+            this.Udaljenost_vrijednost.Size = new System.Drawing.Size(60, 13);
+            this.Udaljenost_vrijednost.TabIndex = 9;
+            this.Udaljenost_vrijednost.Text = "Udaljenost:";
+            // 
+            // Udaljenost_koordinata
+            // 
+            this.Udaljenost_koordinata.Location = new System.Drawing.Point(454, 329);
+            this.Udaljenost_koordinata.Name = "Udaljenost_koordinata";
+            this.Udaljenost_koordinata.ReadOnly = true;
+            this.Udaljenost_koordinata.Size = new System.Drawing.Size(100, 20);
+            this.Udaljenost_koordinata.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 357);
+            this.Controls.Add(this.Udaljenost_koordinata);
+            this.Controls.Add(this.Udaljenost_vrijednost);
+            this.Controls.Add(this.Y2_koordinata);
+            this.Controls.Add(this.Y2_vrijednost);
+            this.Controls.Add(this.X2_koordinata);
+            this.Controls.Add(this.X2_vrijednost);
+            this.Controls.Add(this.Y1_koordinata);
+            this.Controls.Add(this.Y1_vrijednost);
+            this.Controls.Add(this.X1_koordianta);
+            this.Controls.Add(this.X1_vrijednost);
+            this.Controls.Add(this.Koordinatni_sustav);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Koordinatni_sustav;
+        private System.Windows.Forms.Label X1_vrijednost;
+        private System.Windows.Forms.TextBox X1_koordianta;
+        private System.Windows.Forms.Label Y1_vrijednost;
+        private System.Windows.Forms.TextBox Y1_koordinata;
+        private System.Windows.Forms.Label X2_vrijednost;
+        private System.Windows.Forms.TextBox X2_koordinata;
+        private System.Windows.Forms.Label Y2_vrijednost;
+        private System.Windows.Forms.TextBox Y2_koordinata;
+        private System.Windows.Forms.Label Udaljenost_vrijednost;
+        private System.Windows.Forms.TextBox Udaljenost_koordinata;
+    }
+}
+
---
+++ udaljenost između dviju točaka/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udaljenost_između_dviju_točaka
+{
+    public partial class Form1 : Form
+    {
+        int x_start=0, y_start=0;
+        int x_kraj=0, y_kraj=0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Koordinatni_sustav_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                x_start = e.X;
+                y_start = e.Y;
+                X1_koordianta.Text = x_start.ToString();
+                Y1_koordinata.Text = y_start.ToString();
+                Crtaj(true, false);
+            }
+            
+        }
+        private void Koordinatni_sustav_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                x_kraj = e.X;
+                y_kraj = e.Y;
+                double rezultat = Math.Sqrt(Math.Pow(x_kraj - x_start, 2) + Math.Pow(y_kraj - y_start, 2));
+                Y2_koordinata.Text = y_kraj.ToString();
+                X2_koordinata.Text = x_kraj.ToString();
+                Udaljenost_koordinata.Text = rezultat.ToString();
+                Crtaj(false, true);
+                x_start = 0;
+                y_start = 0;
+                x_kraj = 0;
+                y_kraj = 0;
+            }
+        }
+
+        private void Koordinatni_sustav_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        private void Crtaj(bool mouse_down, bool mouse_up)
+        {
+            Graphics graf = Koordinatni_sustav.CreateGraphics();
+            Pen olovka = new Pen(Color.Black);
+
+            if (mouse_down == true && mouse_up == false)
+            {
+                graf.DrawLine(olovka, x_start + 5, y_start + 5, x_start - 5, y_start - 5);
+                graf.DrawLine(olovka, x_start - 5, y_start + 5, x_start + 5, y_start - 5);
+            }
+            else if (mouse_down == false && mouse_up == true)
+            {
+                graf.DrawLine(olovka, x_kraj + 5, y_kraj + 5, x_kraj - 5, y_kraj - 5);
+                graf.DrawLine(olovka, x_kraj - 5, y_kraj + 5, x_kraj + 5, y_kraj - 5);
+                graf.DrawLine(olovka, x_start, y_start, x_kraj, y_kraj);
+            }
+        }
+
+        
+    }
+}
